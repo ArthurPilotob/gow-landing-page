7ed6ac633f7b554cb326fcaf93002221c653f0bb
Arthur Piloto Bastos <arthur.opiloto@gmail.com>
2025-10-02 15:08:44 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
+// Menu hamburguer
+
 const menuHamburguer = document.getElementById('menu-hamburguer');
 const navLinks = document.getElementById('nav-links');
-// Adiciona os elementos que iremos usar (menu hambuguer e seus links)
+
 menuHamburguer.addEventListener('click', () => {
     navLinks.classList.toggle('ativo');
 });
-// Faz abrir o menu hamburguer
+// Abre o menu hamburguer quando clicado
+
 document.addEventListener('click', (event) => {
     const isMenuAtivo = navLinks.classList.contains('ativo');
-
     const isClickFora = !navLinks.contains(event.target) && !menuHamburguer.contains(event.target);
 
     if (isMenuAtivo && isClickFora) {
         navLinks.classList.remove('ativo');
     }
 });
-// Verificação de um click fora do menu sanduiche, para fecha-lo
+// Fecha o menu hamburguer em qualquer click fora da sua box
+
+
+// Modais
+
+const botoesAbrirModal = document.querySelectorAll('[data-modal-id]');
+const botoesFecharModal = document.querySelectorAll('[data-modal-close]');
+
+botoesAbrirModal.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const modalId = botao.getAttribute('data-modal-id');
+        const modal = document.getElementById(modalId);
+        
+        modal.classList.add('ativo');
+    });
+});
+// Faz o modal abrir ao clicar em ver detalhes
+botoesFecharModal.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const modal = botao.closest('.modal');
+        modal.classList.remove('ativo');
+    });
+});
+// Faz o modal fechar ao clicar no X no canto superior esquerdo
